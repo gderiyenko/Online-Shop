1bda95370b60ded4565a32e049fdff7e344ebba4
gderiyenko <gleb.deriyenko@gmail.com>
2018-05-16 12:58:30 +0300

    init

---
+++ app/Basket.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Basket extends Model
+{
+    public static function getAllById($userId)
+    {
+    	return \DB::select('SELECT p.*, pt.name as type_name, bi.count FROM products p, basket_infos bi,product_types pt
+    	 WHERE (p.type_id = pt.id) AND (p.id = bi.product_id) AND (bi.status = ?) AND (bi.count > ?) AND (bi.user_id = ?) ;', [1, 0, $userId]);
+    }
+
+    public static function getSumCost($userId)
+    {
+    	return \DB::select('SELECT sum(p.price*bi.count) as total FROM products p, basket_infos bi
+    	 WHERE (p.id = bi.product_id) AND (bi.status = ?) AND (bi.count > ?) AND (bi.user_id = ?) ;', [1, 0, $userId]);
+    }
+
+    public function Status(){
+	    return $this->belongsTo(BasketStatus::class);
+	}
+
+	public static function addToBasketInfo($userId, $productId){
+		$rawId = \DB::select('SELECT id FROM basket_infos bi WHERE (bi.user_id = ?) and (bi.product_id = ?) AND (bi.status = ?);', [$userId, $productId, 1]);
+		if (count($rawId) == 0){
+			$date = new \DateTime();
+			echo $date->format('Y-m-d H:i:s') . "\n";
+			\DB::table('basket_infos')->insertGetId(
+			    [
+			     'user_id' => $userId,
+			     'product_id' => $productId,
+			     'created_at' => $date->format('Y-m-d H:i:s'),
+			     'count' => 1,
+			     'status' => 1
+			    ]
+			);
+		}
+		else
+			\DB::select('UPDATE basket_infos bi SET bi.count = bi.count+1 WHERE (bi.user_id = ?) AND (bi.product_id = ?);', 
+				[$userId, $productId]);
+		return 1;
+	}
+
+	public static function deleteOneFromBasketInfo($userId, $productId){
+		\DB::select('UPDATE basket_infos bi SET bi.count = bi.count-1 WHERE (bi.user_id = ?) AND (bi.product_id = ?) AND (bi.status = ?);',
+			[$userId, $productId, 1]);
+	}
+
+	public static function deleteFromBasketInfo($userId, $productId){
+		\DB::select('UPDATE basket_infos bi SET bi.status = 2 WHERE (bi.user_id = ?) AND (bi.product_id = ?) AND (bi.status = ?);', 
+			[$userId, $productId, 1]);
+	}
+
+	public static function deleteBasket($userId){
+		\DB::select('UPDATE basket_infos bi SET bi.status = 2 WHERE (bi.user_id = ?) AND (bi.status = ?);', 
+			[$userId, 1]);
+	}
+	public static function processing($userId)
+	{
+		$date = new \DateTime();
+		\DB::select('UPDATE basket_infos bi SET bi.status = 3 WHERE (bi.user_id = ?) AND (bi.status = ?) AND (bi.count > 0);', 
+			[$userId, 1]);
+		\DB::select('UPDATE baskets b SET b.status = 3, b.paid_at = ? WHERE (b.user_id = ?) AND (b.status = ?);', 
+			[$date->format('Y-m-d H:i:s'),$userId, 1]);
+	}
+	
+
+}
---
+++ app/Http/Controllers/BasketController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Basket;
+use App\Product;
+use App\User;
+use App\ProductType;
+use App\BookingQuery;
+
+class BasketController extends Controller
+{
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function list()
+    {
+    	$userId = \Auth::id();
+    	$models = Basket::getAllById($userId);
+        $QueryRequest = BookingQuery::getAllUserQueries($userId);
+        $summaryCostOfProducts = Basket::getSumCost($userId);
+        return view('basket.list', 
+                [
+                    "userProducts"  => $models, 
+                    "allQueries"    => $QueryRequest,
+                    "sumCost"       => $summaryCostOfProducts[0]->total
+                ]);
+    }
+    
+    public function addOne(){
+        $userId = \Auth::id();
+        $json = $_GET['data'];
+        $productId = json_decode($json, true);
+        return Basket::addToBasketInfo($userId, $productId);
+    }
+
+    public function deleteOne(){
+        $userId = \Auth::id();
+        $json = $_GET['data'];
+        $productId = json_decode($json, true);
+        return Basket::deleteOneFromBasketInfo($userId, $productId);
+    }
+
+    public function buyBasket(){
+        $userId = \Auth::id();
+        $templateName = $_GET['data'];
+        var_dump($templateName);
+        die();
+        //basketQuery
+        return BookingQuery::addToQuery($userId, $templateName);
+    }
+
+    public function deleteAllById(){
+        $userId = \Auth::id();
+        $json = $_GET['data'];
+        $productId = json_decode($json, true);
+        return Basket::deleteFromBasketInfo($userId, $productId);
+    }
+
+    public function delete(){
+        $userId = \Auth::id();
+        return Basket::deleteBasket($userId);
+    }
+}
